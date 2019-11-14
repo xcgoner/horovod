bb382738d1a8a8a81ddcad9bf267ad7cecce8007
cx2 <cx2@illinois.edu>
2019-11-14 17:56:40 -0600

    local sgd

--- horovod/mxnet/mpi_ops.cc
+++ horovod/mxnet/mpi_ops.cc
@@ -236,7 +236,7 @@ extern "C" int horovod_mxnet_allreduce_async(NDArray* input, NDArray* output,
   // std::cout << "horovod_mxnet_allreduce_async: " << local_reduction << std::endl;
   // must send default rank=-1
   PushHorovodOperation(OperationType::ALLREDUCE, input, output,
-                       name, priority, -1
+                       name, priority, -1,
                        local_reduction, cross_only);
 #endif
 
